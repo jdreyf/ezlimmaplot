72cd863d2b3aa0a1604f5c7c83d85e0b419d8a8e
jonathan dreyfuss <jdreyf@gmail.com>
2018-05-25 12:53:48 -0400

    add extract_prefix tests

--- R/extract_prefix.R
+++ R/extract_prefix.R
@@ -3,18 +3,19 @@
 #' Extract prefixes from names, presumably colnames.
 #'
 #' @param nm Character vector of names to search.
-#' @param suffix Character string to search for at end of \code{cnames}.
-#' @param sep Separator in \code{cnames} preceeding \code{suffix}. If \code{NA}, elements of \code{nm} must
-#' match \code{suffix} exactly.
+#' @param suffix Character string to search for at end of \code{nm}.
+#' @param sep Separator in \code{nm} preceeding \code{suffix}. One of \code{'.', '_', NA}, If \code{NA}, elements
+#' of \code{nm} must match \code{suffix} exactly.
 #' @return A character vector of prefixes. If there is no prefix and no sep, \code{NA} is returned.
 
-extract_prefix <- function(nm, suffix, sep='.'){
+extract_prefix <- function(nm, suffix='p', sep='.'){
+  stopifnot(sep %in% c(".", "_")|is.na(sep))
   if (!is.na(sep)){
     patt <- paste0('\\', sep, '(', suffix, ')$')
     ind <- grep(pattern=patt, x=nm)
     prefix.v <- sub(patt, '', nm[ind])
   } else {
-    patt <- paste0("^", suffix, '$')
+    patt <- paste0('^', suffix, '$')
     ind <- grep(pattern=patt, x=nm)
     #could return string of NAs
     #NA instead of "" distinguishes "p" from ".p"
--- tests/testthat/test_extract_prefix.R
+++ tests/testthat/test_extract_prefix.R
@@ -1,16 +1,34 @@
 context("extract prefix")
 
-
-test_that("extract_prefix", {
-  expect_error(extract_prefix(nm="p", suffix="p"))
-  expect_true(is.na(extract_prefix(nm="p", suffix="p", sep=NA)))
-
-  expect_equal(extract_prefix(nm=".p", suffix="p"), "")
-  expect_equal(extract_prefix(nm=c("x.p", "x.fdr"), suffix="p"), "x")
-  expect_error(extract_prefix(nm=c("x.p", "x.fdr"), suffix="p", sep=NA))
-
-  expect_error(extract_prefix(nm=c("x_p", "x_fdr"), suffix="p", sep="."))
-  expect_equal(extract_prefix(nm=c("x_p", "x_fdr"), suffix="p", sep="_"), "x")
-
+test_that("nm", {
   expect_equal(extract_prefix(nm=c("x.y.p", "x.y.p", "x.p.fdr"), suffix="p"), c("x.y", "x.y"))
+  expect_error(extract_prefix(nm=""))
+  expect_error(extract_prefix(nm=NA))
+  expect_error(extract_prefix(nm=NULL))
+  expect_equal(extract_prefix(nm=".p", suffix="p"), "")
+  expect_error(extract_prefix(nm="_p", suffix="p"))
+})
+
+test_that("suffix", {
+  expect_error(extract_prefix(nm=c("x.y.p", "x.y.p", "x.p.fdr"), suffix=""))
+  expect_error(extract_prefix(nm=c("x.y.p", "x.y.p", "x.p.fdr"), suffix=NA))
+  expect_error(extract_prefix(nm=c("x.y.p", "x.y.p", "x.p.fdr"), suffix=NULL))
+})
+
+test_that("sep", {
+  expect_error(extract_prefix(nm=c("x.y.p", "x.y.p", "x.p.fdr"), suffix="p", sep=".."))
+  expect_error(extract_prefix(nm=c("x.y.p", "x.y.p", "x.p.fdr"), suffix="p", sep=NA))
+  expect_true(is.na(extract_prefix(nm=c("x.y.p", "x.y.p", "p"), suffix="p", sep=NA)))
+  expect_true(is.na(extract_prefix(nm="p", suffix="p", sep=NA)))
+  expect_error(extract_prefix(nm=c("x.y.p", "x.y.p", "p"), suffix="p", sep="NA"))
+  expect_error(extract_prefix(nm=c("x.y.p", "x.y.p", "x.p.fdr"), suffix="p", sep=NULL))
+  expect_error(extract_prefix(nm=c("x.y..p", "x.y..p", "x.p..fdr"), suffix="p", sep=".."))
+
+  expect_equal(extract_prefix(nm=c("x_p", "x_fdr"), suffix="p", sep="_"), "x")
+  expect_error(extract_prefix(nm=c("x_p", "x_fdr"), suffix="p", sep="."))
+})
+
+test_that("integration", {
+  expect_error(extract_prefix(nm="p", suffix="p"))
+  expect_equal(extract_prefix(nm=c("x.p", "x.fdr"), suffix="p"), "x")
 })
