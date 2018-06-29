66bd5e7ff71c0e9ae6abd8cdeb394881c48ea096
Grace Daher <grace@daher.net>
2018-06-29 10:54:26 -0400

    error tests added for multi_volcano

--- tests/testthat/test_multi_volcano.R
+++ tests/testthat/test_multi_volcano.R
@@ -11,3 +11,15 @@ test_that("vdiffr", {
   vdiffr::expect_doppelganger(title="vol3", fig=mvol2[[1]])
   vdiffr::expect_doppelganger(title="vol4", fig=mvol2[[2]])
 })
+
+test_that("error message fires if no logFC columns, or no p-value/FDR columns in tab",{
+ cols <-  grep(paste0('\\', ".", 'logFC$'), colnames(res.df))
+ res.df2 <- res.df[,-c(cols)]
+ expect_error(multi_volcano(tab=res.df2, name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1, type.sig="FDR", same.scale = TRUE,
+                            cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), lab.col='Gene.Symbol'))
+ res.df3 <- res.df[,-c(grep(paste0('\\', ".", 'FDR'), colnames(res.df)))]
+ expect_error(multi_volcano(tab=res.df3, name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1, type.sig="FDR", same.scale = TRUE,
+                            cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), lab.col='Gene.Symbol'))
+
+})
+
