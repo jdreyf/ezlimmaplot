3ffe119dfffbb57d43c3e2039be5fc97c1bf3e55
jonathan dreyfuss <jdreyf@gmail.com>
2019-02-25 18:23:41 -0500

    update extract_prefix

--- R/signif_hist.R
+++ R/signif_hist.R
@@ -4,9 +4,10 @@
 #'
 #' @param p.suffix Suffix for p-value columns. P-value column names cannot be duplicated.
 #' @param fdr.suffix Suffix for FDR columns. Set to \code{NA} if no FDR columns. FDR column names cannot be duplicated.
-#' @param sep Separator for column names before \code{p} or \code{FDR}.
+#' @param sep Separator for column names before \code{p} or \code{FDR}, passed to \code{\link[ezlimma]{extract_prefix}}.
+#' If not found, it is assumed to be \code{NA}.
 #' @param pi0 Logical indicating if proportion of null hypotheses should be calculated per p-value histogram. If
-#' \code{TRUE}, \code{limma::propTrueNull} with \code{method="convest"} is used, so \code{limma} package is needed.
+#' \code{TRUE}, \code{\link[limma]{propTrueNull}} with \code{method="convest"} is used.
 #' @inheritParams ezheat
 #' @inheritParams ezvenn
 #' @details Some p-value columns must be identifiable using \code{p.suffix}. If \code{!is.na(fdr.suffix)}, FDR
@@ -15,8 +16,10 @@
 #' @export
 
 #assume each comparison has a p-value & q-value column unless fdr.suffix=NA
-#could allow for no prefix, ie colnames(tab)=c('p', 'FDR')
-signif_hist <- function(tab, p.suffix='p', fdr.suffix='FDR', sep='.', pi0 = FALSE, name='signif_hist', plot=TRUE){
+#could allow for no prefix, ie colnames(tab)=c("p", "FDR")
+signif_hist <- function(tab, p.suffix="p", fdr.suffix="FDR", sep=".", pi0 = FALSE, name="signif_hist", plot=TRUE){
+  stopifnot(nrow(tab) > 0, ncol(tab) > 0, !is.null(colnames(tab)))
+
   prefix.v <- extract_prefix(colnames(tab), suffix=p.suffix, sep=sep)
   if (any(duplicated(prefix.v))) stop("p-value column names are duplicated.")
   if (is.na(prefix.v[1])){
@@ -39,7 +42,7 @@ signif_hist <- function(tab, p.suffix='p', fdr.suffix='FDR', sep='.', pi0 = FALS
 
   #set name=NA to turn off pdf
   if (plot){
-    if (!is.na(name)){ grDevices::pdf(paste0(name, '.pdf')) }
+    if (!is.na(name)){ grDevices::pdf(paste0(name, ".pdf")) }
     graphics::par(mfrow=c(2,2))
     for (ind.tmp in 1:length(p.cols)){
       prefix <- prefix.v[ind.tmp]
@@ -50,15 +53,15 @@ signif_hist <- function(tab, p.suffix='p', fdr.suffix='FDR', sep='.', pi0 = FALS
         if (!requireNamespace("limma", quietly = TRUE)){
           stop("Package 'limma' needed to estimate pi0. Please install it.", call. = FALSE)
         }
-        prop.null <- limma::propTrueNull(tab[,p.col], method = 'convest')
-        subtitle <- paste('Proportion of True Null = ', signif(prop.null, 3))
+        prop.null <- limma::propTrueNull(tab[,p.col], method = "convest")
+        subtitle <- paste("Proportion of True Null = ", signif(prop.null, 3))
       }
-      graphics::hist(tab[,p.col], xlab='P-value', main=prefix, sub = subtitle)
+      graphics::hist(tab[,p.col], xlab="P-value", main=prefix, sub = subtitle)
 
       if (!is.na(fdr.suffix)){
         fdr.col <- fdr.cols[ind.tmp]
         stopifnot(length(fdr.col)==1)
-        graphics::hist(tab[,fdr.col], xlab='FDR', main=prefix)
+        graphics::hist(tab[,fdr.col], xlab="FDR", main=prefix)
       }
     }
     if (!is.na(name)){ grDevices::dev.off() }
