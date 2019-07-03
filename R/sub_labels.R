3bd4a12c820e2bd18f679ff2544c89f5277d6e52
jonathan dreyfuss <jdreyf@gmail.com>
2019-07-02 20:28:36 -0400

    test sub_labels

--- R/sub_labels.R
+++ R/sub_labels.R
@@ -1,20 +1,23 @@
-#' Replace names with non-missing labels
+#' Replace names with non-missing label.v
 #'
-#' Replace names, esp. rownames, with non-missing labels.
+#' Replace names, esp. rownames, with non-missing label.v. \code{nms} and \code{label.v} must be of same length,
+#' and \code{nms} must not have \code{NA}s.
 #'
 #' @param nms Vector of names, e.g. rownames.
-#' @param labels Vector of labels, e.g. \code{labrows}.
+#' @param label.v Vector of label.v, e.g. \code{labrows}.
 #' @inheritParams ezheat
 #' @return Vector of replaced rownames; vector's names are rownames
 
 # currently unused
-sub_labels <- function(nms, labels, na.lab=c("---", "")){
+sub_labels <- function(nms, label.v, na.lab=c("---", "")){
+  stopifnot(length(nms)==length(label.v), !is.na(nms), !(nms %in% na.lab))
+
   ret <- stats::setNames(nms, nm=nms)
-  na.sym.ind <- which(is.na(labels) | labels %in% na.lab)
+  na.sym.ind <- which(is.na(label.v) | label.v %in% na.lab)
   if (length(na.sym.ind) > 0){
-    ret[-na.sym.ind] <- labels[-na.sym.ind]
+    ret[-na.sym.ind] <- label.v[-na.sym.ind]
   } else {
-    ret <- labels
+    ret <- label.v
   }
   ret
 }
--- tests/testthat/test-sub_labels.R
+++ tests/testthat/test-sub_labels.R
@@ -1,77 +1,47 @@
-context(sub_labels)
-test_that("Labels replace names", {
+context("sub_labels")
 
-  month.days<-c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-  x <- c(1:10)
-  y <- letters
+test_that("Labels replace names", {
+  month.days <- c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+  x <- 1:12
+  y <- letters[x]
+  expect_equal(sub_labels(month.days, x ,'-'), x)
+  expect_equal(sub_labels(month.days, y, '-'), y)
+})
+
+test_that("When x includes negative numbers", {
+  n <- 22
+  month.days <- rnorm(n=n)
+  x <- c(-1,NA,2,NA,3,NA,4,NA,NA,5,6,7,8,9,10,NA,NA,NA,NA,NA,11,12)
+  y <- letters[1:n]
   # Test for checking length when integers is available to be replaced.
   #expect_length (length(sub_labels(month.days,x,'-')), 1)
+  expect_error(sub_labels(x, month.days, '-'))
   #Test for known output
-  expect_equal(sub_labels(month.days,x,'-'), (c(1 :10)))
-  #Test for checking the length when characters are present to be replaced.
-  #expect_length ((sub_labels(month.days,y,'-')), 26)
+  expect_equal(sub_labels(month.days, y, '-'), y)
 })
 
-  test_that("When x includes negative numbers", {
+test_that("x should have some value except NA", {
+  month.days <- c(31, 28, 31, 30, 31, 30,31,31,30,31,30,31)
+  x <- rep(NA, length(month.days))
 
-    month.days <- c(31, 28, 31, 30, 31, 30,31,31,30,31,30,31)
-    x <- c(-1,NA,2,NA,3,NA,4,NA,NA,5,6,7,8,9,10,NA,NA,NA,NA,NA,11,12)
-    y <- letters
-    # Test for checking length when integers is available to be replaced.
-    #expect_length (length(sub_labels(month.days,x,'-')), 1)
-    expect_warning (sub_labels(month.days,x,'-'))
-
-
-    #Test for known output
-    expect_equal(sub_labels(month.days,y,'-'), y)
-    #Test for checking the length when characters are present to be replaced.
-    #expect_length ((sub_labels (month.days, y,'-')), 26)
-
+  #Test for any error
+  expect_error(sub_labels(month.days, x, na.lab = c("--", 31)))
+  # Test that all the arguments are defined
+  expect_error(sub_labels(month.days, x, na.lab))
 })
-  test_that("x should have some value except NA", {
 
-    month.days <- c(31, 28, 31, 30, 31, 30,31,31,30,31,30,31)
-    x <- c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
-
-    #Test for any error
-    expect_that(sub_labels(month.days, x, na.lab = c("---","")),throws_error())
-    # Test that all the arguments are being used.
-    expect_that(sub_labels(month.days, x), throws_error())
-    # Test that all the arguments are defined
-    expect_that(sub_labels(month.days, x, na.lab), throws_error())
-  })
-
-  test_that("x should have some value except NA", {
-
-    month.days <- c(31, 28, 31, 30, 31, 30,31,31,30,31,30,31)
-    x <- c(NA, NA, NA, NA, 1)
-    t <- c(31, 28, 31, 30, 1, 1, 1, 1, 1, 1, 1, 1)
-    names(t) <- month.days
-    #Test to check that we receive the named vector
-    expect_named(sub_labels(month.days, x, na.lab = c("---","")))
-    #Test to check that we receive the known value
-    expect_equal(sub_labels(month.days, x, na.lab = c("---","")), t)
-    #Test to check the replacement length
-    expect_that(sub_labels(month.days, x, na.lab = c("1","")), throws_error)
-
-  })
-
-  test_that("x should have some value except NA", {
-
-    month.days <- c(31, 28, 31, 30, 31, 30,31,31,30,31,30,31)
-    x <- c(NA,-1, -2)
-    t <- c(31, -1, -2, -1, -2, -1, -2, -1, -2, -1, -2, -1)
-    names(t) <- month.days
-    r <- c(31, 28, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2)
-    names(r) <- month.days
-    s <- c(31, -1, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1)
-    names(s) <- month.days
-
-    #Test to check that we receive the known value
-    expect_equal(sub_labels(month.days, x, na.lab = c("1","")), t)
-
-    expect_equal(sub_labels(month.days, x, na.lab = c("-1","")), r)
-
-    expect_equal(sub_labels(month.days, x, na.lab = c("-2","")), s)
-  })
-
+test_that("x should have some value except NA", {
+  month.days <- c(31, 28, 31, 30, 31, 30,31,31,30,31,30,31)
+  x <- rep(NA, length(month.days))
+  x[1] <- 1
+  t <- c(31, 28, 31, 30, 1, 1, 1, 1, 1, 1, 1, 1)
+  names(t) <- month.days
+  #Test to check that we receive the named vector
+  expect_named(sub_labels(month.days, x, na.lab = c("---","")))
+  #Test to check that we receive the known value
+  expect_equal(sub_labels(month.days, t, na.lab = c("---","")), t)
+  #Test to check the replacement length
+  expect_error(sub_labels(month.days, x, na.lab = c(31, "")))
+  #all NA
+  expect_equal(sub_labels(month.days, rep(NA, length(month.days))), setNames(month.days, nm=month.days))
+})
