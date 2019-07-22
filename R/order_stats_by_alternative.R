c2022a37badb0bfb567a79820726892db896d30a
jonathan dreyfuss <jdreyf@gmail.com>
2019-07-22 19:42:03 -0400

    add match.arg alternative

--- R/order_stats_by_alternative.R
+++ R/order_stats_by_alternative.R
@@ -2,10 +2,12 @@
 #'
 #' Order values based on alternative hypothesis.
 #'
-#' @param stat.v Numeric vector of statistics to order; preferably a named vector.
+#' @param stat.v Numeric vector of statistics to order; preferably with names. May include \code{NA}s, which are
+#' last in \code{\link{order}}.
 #' @inheritParams ezlimma::roast_contrasts
 
-order_stats_by_alternative <- function(stat.v, alternative){
+order_stats_by_alternative <- function(stat.v, alternative=c("two.sided", "greater", "less")){
+  alternative <- match.arg(alternative)
   # want highest impact; order puts NAs last
   top.nodes <- switch(alternative,
                       greater = stat.v[order(stat.v, decreasing = TRUE)],
