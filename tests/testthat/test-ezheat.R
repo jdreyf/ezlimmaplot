40004a612b00422eb877e4bca0e3c018ec907655
jonathan dreyfuss <jdreyf@gmail.com>
2019-11-20 23:16:53 -0500

    add tests from gupta on upwork

--- tests/testthat/test-ezheat.R
+++ tests/testthat/test-ezheat.R
@@ -28,6 +28,87 @@ test_that("sc", {
   expect_equal(ezh.sc, base.sc)
 })
 
+test_that("non vdiffr", {
+  #pheatmap creates two pages, but vdiffr supports only one, so using returned gtable
+  lr <- sub_labels(topgenes, res.df[topgenes, "Gene.Symbol"])
+  ret <- ezheat(M[topgenes,], labrows = lr, pheno.df=pheno.df, name=NA)
+  expect_equal(rownames(ret$mat), rownames(as.data.frame(lr)))
+  expect_equal(colnames(ret$mat), row.names(pheno.df))
+  expect_equal(rownames(ret$mat), topgenes)
+  expect_equal(ret$mat, t(scale(x=t(M[topgenes,]), scale=FALSE)))
+  expect_equal(ret$gtable$grobs[[1]]$label, paste("Centered","Log2 Expression"))
+
+  lr <- sub_labels(topgenes, res.df[topgenes, "Gene.Symbol"])
+  ret <- ezheat(M[topgenes,], labrows = lr, pheno.df=pheno2, name=NA, sc="z")
+  expect_equal(ret$mat, t(scale(x=t(M[topgenes,]), center=TRUE, scale=TRUE)))
+  expect_equal(ret$gtable$grobs[[1]]$label, paste("Z-scored","Log2 Expression"))
+
+  lr <- sub_labels(topgenes, res.df[topgenes, "Gene.Symbol"])
+  ret <- ezheat(object=res.df[topgenes, 1:2]+10, labrows = lr, name=NA, sc="none",
+                  stat.tab=res.df[topgenes, c("First3.p", "Last3.p")])
+  mat <- res.df[topgenes, 1:2]+10
+  expect_equal(as.data.frame(ret$mat), as.data.frame(mat) )
+  expect_equal(ret$gtable$grobs[[1]]$label, "Log2 Expression")
+
+  lr <- sub_labels(topgenes, res.df[topgenes, "Gene.Symbol"])
+  ret <- ezheat(M[topgenes,], labrows = lr, pheno.df=pheno2, name=NA,
+                  reorder_rows = TRUE, reorder_cols = TRUE)
+  expect_setequal(rownames(ret$mat), rownames(as.data.frame(lr)))
+  expect_setequal(colnames(ret$mat), row.names(pheno.df))
+  expect_setequal(rownames(ret$mat), topgenes)
+  expect_setequal(ret$mat, t(scale(x=t(M[topgenes,]), scale=FALSE)))
+  expect_equal(ret$gtable$grobs[[1]]$label, paste("Centered","Log2 Expression"))
+
+  #verify by eye that columns & rows are reordered; title is only "Log2 Expression"
+  lr <- sub_labels(topgenes, res.df[topgenes, "Gene.Symbol"])
+  ret <- ezheat(M[topgenes,], labrows = lr, pheno.df=pheno.df, name=NA, sc="none",
+                  reorder_rows = TRUE, reorder_cols = TRUE, labcols=letters[1:6])
+  expect_setequal(rownames(ret$mat), rownames(as.data.frame(lr)))
+  expect_setequal(colnames(ret$mat), row.names(pheno.df))
+  expect_setequal(rownames(ret$mat), topgenes)
+  expect_setequal(ret$mat, t(M[topgenes,]))
+  expect_equal(ret$gtable$grobs[[1]]$label, "Log2 Expression")
+
+  #verify only unique rows
+  ret <- ezheat(rbind(M[topgenes,], M[topgenes,]), name=NA, unique.rows=TRUE, sc="none")$mat
+  expect_equal(ret, unique(rbind(M[topgenes,], M[topgenes,])))
+
+  #verify only top 10 rows are included
+  ret <- ezheat(M[topgenes,], name=NA, sc="none", ntop=10)$mat
+  expect_equal(ret, head(M[topgenes,], n=10))
+
+  #verify only top 10 rows are included after taking unique set of rows
+  ret <- ezheat(rbind(M[topgenes,], M[topgenes,]), name=NA, unique.rows=TRUE, sc="none", ntop=10)$mat
+  expect_equal(ret, head(unique(rbind(M[topgenes,], M[topgenes,])), n=10))
+
+  #verify rows are reordering
+  ret <- ezheat(M[topgenes,], name=NA, sc="none", reorder_rows=TRUE)$mat
+  expect_setequal(row.names(ret), row.names(M[topgenes,]))
+
+  #verify columns are reordering
+  ret <- ezheat(M[topgenes,], name=NA, sc="none", reorder_cols=FALSE, reorder_rows=TRUE)$mat
+  expect_setequal(colnames(ret), colnames(M[topgenes,]))
+
+  #verify rows and columns are reordering
+  ret <- ezheat(M[topgenes,], name=NA, sc="none", reorder_cols=FALSE, reorder_rows=TRUE)$mat
+  expect_setequal(row.names(ret), row.names(M[topgenes,]))
+  expect_setequal(colnames(ret), colnames(M[topgenes,]))
+
+  #verify any value >=1 in M is clipped to 1
+  ret <- ezheat(M[topgenes,], name=NA, sc="none", clip=1)$mat
+  expect_equal(max(ret), 1)
+ })
+
+test_that("non vdiffr low res", {
+  # low res heat w/ clusters & w/o rowlabs
+  cl.df <- data.frame(cl=LETTERS[as.numeric(sub("gene", "", rownames(M))) %% 5 + 1])
+  rownames(cl.df) <- rownames(M)
+  ret <- ezheat(M, pheno.df=pheno.df, labrows = "", name=NA, annotation_row = cl.df)
+  expect_equal(ret$mat, t(scale(x=t(M), scale=FALSE)))
+  expect_equal(colnames(ret$mat), row.names(pheno.df))
+  expect_equal(ret$gtable$grobs[[1]]$label, paste("Centered","Log2 Expression"))
+})
+
 test_that("vdiffr", {
   #pheatmap creates two pages, but vdiffr supports only one, so using returned gtable
   ezh <- function(){
--- tests/testthat/test-ezvolcano.R
+++ tests/testthat/test-ezvolcano.R
@@ -1,44 +1,5 @@
 context("ezvolcano")
 
-test_that("vdiffr", {
-  ezvol <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
-                                cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"))
-  vdiffr::expect_doppelganger(title="vol", fig=ezvol)
-
-  #no lab column, so no labs
-  ezvol2 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, cut.lfc=1, cut.sig=0.01, cut.color = "green",
-                                 type.sig = "FDR")
-  vdiffr::expect_doppelganger(title="vol2", fig=ezvol2)
-
-  ezvol3 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
-                                 cut.sig=0.01, cut.color = "black", up.ann.color = "red", x.bound = 3, y.bound=30)
-  vdiffr::expect_doppelganger(title="vol3", fig=ezvol3)
-
-  #check that no error with no cut or ann points
-  ezvol4 <- function() ezvolcano(tab=res.df[-1,], comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
-                                 cut.sig=0.01, cut.color = "blue", up.ann.color = "red", x.bound = 3, y.bound=30)
-  vdiffr::expect_doppelganger(title="vol4", fig=ezvol4)
-
-  #lab.col is NULL, but give pts to annotate
-  ezvol5 <- function() suppressWarnings(ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1,
-                                                  cut.lfc=1, lab.col=NULL, cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25")))
-  vdiffr::expect_doppelganger(title="vol5", fig=ezvol5)
-
-  #shape=1 -> empty dots
-  ezvol6 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
-                                 cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), shape = 1)
-  vdiffr::expect_doppelganger(title="vol6", fig=ezvol6)
-
-  expect_warning(ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
-                           lab.col=NULL, cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), plot=FALSE))
-
-  ezvol7 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
-                                 cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), p05.line = TRUE)
-  vdiffr::expect_doppelganger(title="vol7", fig=ezvol7)
-
-  expect_warning(ezvolcano(tab=res.df, comparison = "First3", name=NA, type.sig="FDR", p05.line = TRUE, plot=FALSE))
-})
-
 test_that("non-vdiffr", {
   set.seed(0)
   ezvol <- ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
@@ -125,4 +86,44 @@ test_that("non-vdiffr", {
   expect_equal(ggplot2::ggplot_build(ezvol7)$data[[4]]$yintercept[1], -log10(0.05))
   # expect_true(any(ggplot2::ggplot_build(ezvol7)$data[[3]]$y == -log10(ezvol7$data["gene25", "First3.p"])))
   expect_equal(ggplot2::ggplot_build(ezvol7)$data[[1]]$y[1], -log10(ezvol7$data["gene1", "First3.p"]))
+})
+
+test_that("vdiffr", {
+  ezvol <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
+                                cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"))
+  vdiffr::expect_doppelganger(title="vol", fig=ezvol)
+
+  #no lab column, so no labs
+  ezvol2 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, cut.lfc=1, cut.sig=0.01, cut.color = "green",
+                                 type.sig = "FDR")
+  vdiffr::expect_doppelganger(title="vol2", fig=ezvol2)
+
+  ezvol3 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
+                                 cut.sig=0.01, cut.color = "black", up.ann.color = "red", x.bound = 3, y.bound=30)
+  vdiffr::expect_doppelganger(title="vol3", fig=ezvol3)
+
+  #check that no error with no cut or ann points
+  ezvol4 <- function() ezvolcano(tab=res.df[-1,], comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
+                                 cut.sig=0.01, cut.color = "blue", up.ann.color = "red", x.bound = 3, y.bound=30)
+  vdiffr::expect_doppelganger(title="vol4", fig=ezvol4)
+
+  #lab.col is NULL, but give pts to annotate
+  ezvol5 <- function() suppressWarnings(ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1,
+                                                  cut.lfc=1, lab.col=NULL, cut.sig=0.01, cut.color = "green",
+                                                  ann.rnames=c("gene1", "gene25")))
+  vdiffr::expect_doppelganger(title="vol5", fig=ezvol5)
+
+  #shape=1 -> empty dots
+  ezvol6 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
+                                 cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), shape = 1)
+  vdiffr::expect_doppelganger(title="vol6", fig=ezvol6)
+
+  expect_warning(ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
+                           lab.col=NULL, cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), plot=FALSE))
+
+  ezvol7 <- function() ezvolcano(tab=res.df, comparison = "First3", name=NA, ntop.sig = 1, ntop.lfc = 1, cut.lfc=1,
+                                 cut.sig=0.01, cut.color = "green", ann.rnames=c("gene1", "gene25"), p05.line = TRUE)
+  vdiffr::expect_doppelganger(title="vol7", fig=ezvol7)
+
+  expect_warning(ezvolcano(tab=res.df, comparison = "First3", name=NA, type.sig="FDR", p05.line = TRUE, plot=FALSE))
 })
