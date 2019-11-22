5303c070b166cfa450006b29a6e9aaefc352d86e
jonathan dreyfuss <jdreyf@gmail.com>
2019-11-22 12:15:44 -0500

    Update test-plot_man.R

--- tests/testthat/test-plot_man.R
+++ tests/testthat/test-plot_man.R
@@ -1,7 +1,7 @@
 context("plot_man")
 
 test_that("plot man non-vdiffr", {
-  pm <- plot_man(E=grp, M=M[1,,drop=FALSE], Y=pheno$covar_num, ylab = "Covar", name=NA)
+  pm <- plot_man(E=grp, M=M[1,,drop=FALSE], Y=pheno$covar_num, ylab = "Covar", name="Rplots")
   # test pm$data: To check the location of the point
   expect_equal(pm$data["sample4", "Exprs"], M["gene1", "sample4"])
   # test pm$data
@@ -14,18 +14,18 @@ test_that("plot man non-vdiffr", {
   expect_equal(as.character(pm$data$Exposure), grp)
 
   #test for different labels and title
-  pm <- plot_man(E=grp, M=M[1,,drop=FALSE], Y=pheno$covar_num, xlab = "Covar", main.v="MyTitle", name=NA)
+  pm <- plot_man(E=grp, M=M[1,,drop=FALSE], Y=pheno$covar_num, xlab = "Covar", main.v="MyTitle", name="Rplots")
   expect_equal(pm$labels$title, "MyTitle")
   expect_equal(pm$labels$x, "Covar")
 
   #test for E>2	and color
-  pm <- plot_man(E=c("A1","A1","B2","B2","C3","C3"), M=M[1,,drop=FALSE], Y=pheno$covar_num, manual.color=c("red","orange","cyan"), name=NA)
+  pm <- plot_man(E=c("A1","A1","B2","B2","C3","C3"), M=M[1,,drop=FALSE], Y=pheno$covar_num, manual.color=c("red","orange","cyan"), name="Rplots")
   p.obj <- ggplot_build(pm)
   expect_equal(as.character(p.obj$data[[1]]$colour), c("red","red","orange","orange","cyan","cyan"))
   expect_equal(as.character(pm$data$Exposure), c("A1","A1","B2","B2","C3","C3"))
 
   #test for manual.color list not matching with factors in E
-  expect_error(plot_man(E=c("A1","A1","B2","B2","C3","C3"), M=M[1,,drop=FALSE], Y=pheno$covar_num, manual.color=c("red","orange"), name=NA))
+  expect_error(plot_man(E=c("A1","A1","B2","B2","C3","C3"), M=M[1,,drop=FALSE], Y=pheno$covar_num, manual.color=c("red","orange"), name="Rplots"))
 })
 
 test_that("plot man vdiffr", {
